be57f440822c6997dac7c60c3392a44f43b8db9e
Vasile Boris <boris@espressoprogrammer.com>
2020-07-03 09:08:35 +0300

    Add logging to identify the process on which jobs are run

--- redis/BooksWorker.js
+++ redis/BooksWorker.js
@@ -1,3 +1,4 @@
+const config = require('config');
 const Bull = require('bull');
 
 const BooksRedis = require('../redis/BooksRedis');
@@ -8,7 +9,9 @@ const booksQueue = new Bull('books');
 const booksRedis = new BooksRedis();
 
 booksQueue.process(async (job) => {
+    const { port } = config.get('node');
     const user = job.data.user;
+    console.log(`Store books for ${user} on process running on port ${port}`);
     await booksRedis.storeUserBooks(user);
     await booksQueue.add({ user }, {delay: BOOKS_JOB_SCHEDULE_SECONDS * 1000});
 });
